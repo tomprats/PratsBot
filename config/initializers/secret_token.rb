9c598e18a1af5ecfbbef7ad87e455efd02635ef4
Tom Prats <tprats108@gmail.com>
2014-01-02 18:09:30 -0500

    Rails new PratsBot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PratsBot::Application.config.secret_key_base = 'd79fdc99024a01307ab78e1304069937368957971d86d01924e1f10f5a6d0cf4a723e4338237e1bfc8a3420d29694cdaf486470c57e1cd5501eed343dd13af15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PratsBot::Application.config.session_store :cookie_store, key: '_PratsBot_session'
